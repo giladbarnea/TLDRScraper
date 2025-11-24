f2826a1546f990d73a724b39d444de54e0b2a339
Gilad Barnea <18548473+giladbarnea@users.noreply.github.com>
2025-11-24 15:02:41 +0200

    Remove SSL verification suppression from supabase_client
    Removed SSL verification suppression for httpx and warnings.

--- supabase_client.py
+++ supabase_client.py
@@ -1,11 +1,6 @@
 from supabase import create_client
 import util
 import ssl
-import httpx
-import warnings
-
-# Suppress SSL warnings when verification is disabled
-warnings.filterwarnings('ignore', message='Unverified HTTPS request')
 
 _original_create_default_context = ssl.create_default_context
 
@@ -16,21 +11,6 @@ def _create_unverified_context(*args, **kwargs):
     return context
 
 ssl.create_default_context = _create_unverified_context
-
-# Monkey-patch httpx to disable SSL verification globally
-_original_httpx_client = httpx.Client
-_original_httpx_async_client = httpx.AsyncClient
-
-def _patched_httpx_client(*args, **kwargs):
-    kwargs['verify'] = False
-    return _original_httpx_client(*args, **kwargs)
-
-def _patched_httpx_async_client(*args, **kwargs):
-    kwargs['verify'] = False
-    return _original_httpx_async_client(*args, **kwargs)
-
-httpx.Client = _patched_httpx_client
-httpx.AsyncClient = _patched_httpx_async_client
 
 _supabase_client = None
 
