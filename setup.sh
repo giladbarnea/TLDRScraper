58ef73afc2e31492665633fda057a77c3c6d7ee0
Gilad Barnea <18548473+giladbarnea@users.noreply.github.com>
2025-11-26 08:59:05 +0200

    fix: use npm ci instead of npm install to prevent package-lock.json churn (#292)

--- setup.sh
+++ setup.sh
@@ -347,12 +347,12 @@ function ensure_eza(){
 # }
 
 function build_client(){
-  if { builtin cd client && npm install && npm run build; } then
+  if { builtin cd client && npm ci && npm run build; } then
     message "[$0] Successfully built client"
     builtin cd "$SERVER_CONTEXT_WORKDIR"
     return 0
   else
-    message "[$0][ERROR] Failed to 'cd client && npm install && npm run build'" >&2
+    message "[$0][ERROR] Failed to 'cd client && npm ci && npm run build'" >&2
     builtin cd "$SERVER_CONTEXT_WORKDIR"
     return 1
   fi
